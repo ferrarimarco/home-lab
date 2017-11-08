d99e8f339eac978dd0bef1570fee6b6ec22dc806
Marco Ferrari <ferrari.marco@gmail.com>
2017-11-08 16:44:33 +0100

    Use NetworkManager to configure networks. For #67, #68
    1. Install and enable NetworkManager
    2. Configure network interfaces with NetworkManager
    3. Ensured idempotency when configuring NetworkManager
    4. Completed configuration for dhcp hosts via NetworkManager
    5. Force the use of our default route and dns instead of the hypervisor's

---
+++ scripts/ubuntu/configure-volatile-default-route.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+TEMP=`getopt -o vdm: --long ip-v4-gateway-ip-address:,network-interface: -n 'configure-volatile-default-route' -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+eval set -- "$TEMP"
+
+interface=
+ip_v4_gateway_ip_address=
+
+while true; do
+  case "$1" in
+    -g | --ip-v4-gateway-ip-address ) ip_v4_gateway_ip_address="$2"; shift 2 ;;
+    -i | --network-interface ) interface="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+current_default_gateway="$(ip route | awk '/default/ { print $3 }')"
+echo "Current default gateway for $interface interface: $current_default_gateway. Desired default gateway: $ip_v4_gateway_ip_address"
+if [ "$current_default_gateway" != "$ip_v4_gateway_ip_address" ]; then
+  if [ -n "$current_default_gateway" ]; then
+    echo "Removing default route from $interface interface"
+    ip route del default
+  fi
+  echo "Configuring the default route for $interface interface via $ip_v4_gateway_ip_address gateway"
+  ip route add default via $ip_v4_gateway_ip_address dev $interface
+fi
+
+current_default_gateway="$(ip route | awk '/default/ { print $3 }')"
+echo "Current default gateway for $interface interface: $current_default_gateway"
---
+++ scripts/ubuntu/install-network-manager.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+if [ $(dpkg-query -W -f='${Status}' network-manager 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "Installing NetworkManager"
+  apt-get update
+  apt-get install -y network-manager
+
+  echo "Disabling dnsmasq used by NetworkManager"
+  sed -i '/dnsmasq/d' /etc/NetworkManager/NetworkManager.conf
+
+
+  echo "Reconfigure resolvconf to fix missing symbolic links"
+  dpkg-reconfigure -f noninteractive resolvconf
+fi
+
+ENABLED=$(systemctl status NetworkManager.service|grep 'enabled;' | wc -l)
+if [ "$ENABLED" -ne 1 ];
+then
+  echo "Enabling NetworkManager service"
+  systemctl enable NetworkManager.service
+else
+  echo "NetworkManager service already enabled"
+fi
+
+UP=$(systemctl status NetworkManager.service|grep 'Active: active' | wc -l)
+if [ "$UP" -ne 1 ];
+then
+  echo "Starting NetworkManager service"
+  systemctl start NetworkManager.service
+else
+  echo "NetworkManager service already running"
+fi
