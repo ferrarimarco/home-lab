ce3ff2819762f6630aec202a6636294cd766c0ce
Marco Ferrari <ferrari.marco@gmail.com>
2017-11-16 13:10:00 +0100

    Fall back on /etc/resolv.conf

--- scripts/ubuntu/configure-name-resolution.sh
+++ scripts/ubuntu/configure-name-resolution.sh
@@ -15,11 +15,25 @@ while true; do
   esac
 done
 
+resolvconf_path="/etc/resolvconf/resolv.conf.d/head"
+
+echo "resolv.conf path: $resolvconf_path"
+
+if [ -e "$resolvconf_path" ]; then
+  echo "$resolvconf_path exists"
+else
+  echo "$resolvconf_path not found"
+  resolvconf_path="/etc/resolv.conf"
+  echo "Falling back on $resolvconf_path"
+fi
+
 echo "Removing all previous nameservers"
-sed -i '/^nameserver/d' /etc/resolvconf/resolv.conf.d/head
+sed -i '/^nameserver/d' $resolvconf_path
 
 echo "Setting $ip_v4_dns_nameserver as the preferred DNS server"
-echo "nameserver $ip_v4_dns_nameserver" >> /etc/resolvconf/resolv.conf.d/head
+echo "nameserver $ip_v4_dns_nameserver" >> "$resolvconf_path"
 
-echo "Refreshing /etc/resolv.conf"
-resolvconf -u
+if which resolvconf 2>/dev/null; then
+  echo "Refreshing $resolvconf_path"
+  resolvconf -u
+fi
