b91879fa465b9d739269f459256a06ded9a37b6f
Marco Ferrari <ferrari.marco@gmail.com>
2018-03-30 16:41:46 +0200

    Parametrize interface when configuring networkmanager

--- scripts/linux/configure-network-manager.sh
+++ scripts/linux/configure-network-manager.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-if ! TEMP="$(getopt -o vdm: --long domain:,ip-v4-dns-nameserver:,ip-v4-gateway-ip-address:,ip-v4-host-address:,ip-v4-host-cidr:,network-type: -n 'configure-network-manager' -- "$@")" ; then echo "Terminating..." >&2 ; exit 1 ; fi
+if ! TEMP="$(getopt -o vdm: --long domain:,--interface:,ip-v4-dns-nameserver:,ip-v4-gateway-ip-address:,ip-v4-host-address:,ip-v4-host-cidr:,network-type: -n 'configure-network-manager' -- "$@")" ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$TEMP"
 
 domain=
-interface="$(find /sys/class/net \( -not -name lo -and -not -name 'docker*' -and -not -type d \) -printf "%f\\n" | sort | sed -n '2p')"
+interface=
+_interface="$(find /sys/class/net \( -not -name lo -and -not -name 'docker*' -and -not -type d \) -printf "%f\\n" | sort | sed -n '2p')"
 ip_v4_dns_nameserver=
 ip_v4_gateway_ip_address=
 ip_v4_host_address=
@@ -18,6 +19,7 @@ while true; do
     -d | --ip-v4-dns-nameserver ) ip_v4_dns_nameserver="$2"; shift 2 ;;
     -g | --ip-v4-gateway-ip-address ) ip_v4_gateway_ip_address="$2"; shift 2 ;;
     -h | --ip-v4-host-address ) ip_v4_host_address="$2"; shift 2 ;;
+    -i | --interface ) interface="$2"; shift 2 ;;
     -j | --ip-v4-host-cidr ) ip_v4_host_cidr="$2"; shift 2 ;;
     -s | --domain ) domain="$2"; shift 2 ;;
     -t | --network-type ) network_type="$2"; shift 2 ;;
@@ -25,6 +27,10 @@ while true; do
     * ) break ;;
   esac
 done
+
+if [ "x$interface" = "x" ]; then
+  interface="$_interface"
+fi
 
 echo "Configuring $interface interface with a $network_type configuration"
 
