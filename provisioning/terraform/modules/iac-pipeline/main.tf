0dead6ff0ce4c8a77713fad023f9ab251fd9e8ed
Marco Ferrari <ferrari.marco@gmail.com>
2020-02-28 18:35:06 +0100

    Enable cloudresourcemanager APIs

--- provisioning/terraform/modules/iac-pipeline/main.tf
+++ provisioning/terraform/modules/iac-pipeline/main.tf
@@ -1,3 +1,10 @@
+resource "google_project_service" "cloudresourcemanager-apis" {
+  service = "cloudresourcemanager.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy         = true
+}
+
 resource "google_project_service" "cloudbuild-apis" {
   service = "cloudbuild.googleapis.com"
 
