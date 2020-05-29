9c7581d63b2485ced66e5332ca750e7020d6c782
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-29 18:58:59 +0200

    install deps

--- scripts/linux/ci/initialize-esp-idf.sh
+++ scripts/linux/ci/initialize-esp-idf.sh
@@ -2,4 +2,15 @@
 
 set -e
 
-git clone git@github.com:espressif/esp-idf.git
+INITIAL_PWD="$(pwd)"
+
+git clone --recursive https://github.com/espressif/esp-idf.git
+
+cd esp-idf || exit 1
+./install.sh
+
+# shellcheck disable=SC1091
+. ./export.sh
+
+echo "Changing directory back to $INITIAL_PWD"
+cd "$INITIAL_PWD"
--- scripts/linux/ci/install-dependencies.sh
+++ scripts/linux/ci/install-dependencies.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
 apt-get update
-apt-get install coreutils python3 python3-pip
+apt-get install \
+    bison \
+    ccache \
+    cmake \
+    coreutils \
+    dfu-util \
+    flex \
+    git \
+    gperf \
+    libffi-dev \
+    libssl-dev \
+    ninja-build \
+    python3 \
+    python3-pip \
+    python3-setuptools \
+    wget
