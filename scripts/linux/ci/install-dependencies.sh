2fa9d5fcfd31a87d2056660a9a7c5df6e0194886
Marco Ferrari <ferrari.marco@gmail.com>
2020-06-09 14:55:01 +0200

    Fix cmake URL

--- scripts/linux/ci/install-dependencies.sh
+++ scripts/linux/ci/install-dependencies.sh
@@ -30,7 +30,7 @@ echo "Installing CMake $CMAKE_VERSION..."
 CMAKE_ARCHIVE_NAME=cmake-"$CMAKE_VERSION"-Linux-x86_64.tar.gz
 echo "Downloading $CMAKE_ARCHIVE_NAME"
 
-wget https://github.com/Kitware/CMake/releases/download/v"$CMAKE_VERSION"/"$CMAKE_ARCHIVE_NAME"-Linux-x86_64.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v"$CMAKE_VERSION"/"$CMAKE_ARCHIVE_NAME"
 tar xf "$CMAKE_ARCHIVE_NAME"
 
 PATH="$(pwd)/cmake-$CMAKE_VERSION-Linux-x86_64/bin:$PATH"
