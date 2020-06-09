1022116c75cd03df68aa6cc1d598decfb18eeb7e
Marco Ferrari <ferrari.marco@gmail.com>
2020-06-09 12:46:03 +0200

    CMake

--- scripts/linux/ci/install-dependencies.sh
+++ scripts/linux/ci/install-dependencies.sh
@@ -17,3 +17,12 @@ apt-get install \
     python3-pip \
     python3-setuptools \
     wget
+
+echo "Installing CMake..."
+CMAKE_ARCHIVE_NAME=cmake-"$CMAKE_VERSION".tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v"$CMAKE_VERSION"/"$CMAKE_ARCHIVE_NAME"
+tar xf "$CMAKE_ARCHIVE_NAME"
+cd cmake-"$CMAKE_VERSION" || exit 1
+./bootstrap
+
+cmake --version
