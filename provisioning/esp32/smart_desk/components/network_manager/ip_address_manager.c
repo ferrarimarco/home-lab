56ece8639d12f0f77f49d0030f6baf492c8998f5
Marco Ferrari <ferrari.marco@gmail.com>
2020-06-13 20:42:04 +0200

    Remove unused imports

--- provisioning/esp32/smart_desk/components/network_manager/ip_address_manager.c
+++ provisioning/esp32/smart_desk/components/network_manager/ip_address_manager.c
@@ -1,13 +1,4 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-
-#include "esp_system.h"
-#include "esp_event.h"
 #include "esp_log.h"
-
-#include "lwip/err.h"
-#include "lwip/sys.h"
 
 #include "ip_address_manager.h"
 
