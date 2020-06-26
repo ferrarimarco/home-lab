4cd6b9cf839ab25a3a87f52e6ce3c1220737af0a
Marco Ferrari <ferrari.marco@gmail.com>
2020-06-26 11:59:11 +0200

    Check CMake version

--- scripts/linux/ci/install-dependencies.sh
+++ scripts/linux/ci/install-dependencies.sh
@@ -2,19 +2,19 @@
 
 set -e
 
+CMAKE_VERSION="$1"
+
 if [ -z "${CMAKE_VERSION}" ]; then
     echo 'The CMAKE_VERSION environment variable that specifies the CMake version to install is not defined. Terminating...'
     exit 1
 fi
 
-CURRENT_PWD="$(pwd)"
+echo "Installing CMake $CMAKE_VERSION..."
 
+CURRENT_PWD="$(pwd)"
 echo "PWD: $CURRENT_PWD"
 
 cd "$HOME" || exit 1
-
-CMAKE_VERSION="$1"
-echo "Installing CMake $CMAKE_VERSION..."
 
 CMAKE_ARCHIVE_NAME=cmake-"$CMAKE_VERSION"-Linux-x86_64.tar.gz
 echo "Downloading $CMAKE_ARCHIVE_NAME"
