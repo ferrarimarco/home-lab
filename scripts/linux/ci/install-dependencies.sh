31221164d91560a13cc9647788d9c9c5b1d497cf
Marco Ferrari <ferrari.marco@gmail.com>
2020-06-26 10:54:40 +0200

    Install CMake

--- scripts/linux/ci/install-dependencies.sh
+++ scripts/linux/ci/install-dependencies.sh
@@ -2,24 +2,6 @@
 
 set -e
 
-apt-get update
-apt-get install \
-    bison \
-    ccache \
-    cmake \
-    coreutils \
-    dfu-util \
-    flex \
-    git \
-    gperf \
-    libffi-dev \
-    libssl-dev \
-    ninja-build \
-    python3 \
-    python3-pip \
-    python3-setuptools \
-    wget
-
 CURRENT_PWD="$(pwd)"
 
 cd "$HOME" || exit 1
