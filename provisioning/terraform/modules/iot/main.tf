2180aef6e275dbd56f400552dac6d7bf72cd4531
Marco Ferrari <ferrari.marco@gmail.com>
2020-07-29 11:25:48 +0200

    Don't create an IOT project

--- provisioning/terraform/modules/iot/main.tf
+++ provisioning/terraform/modules/iot/main.tf
@@ -1,58 +1,40 @@
-resource "google_project" "ferrarimarco-iot-project" {
-  billing_account = var.google_project_billing_account_id
-  name            = var.google_project_id
-  project_id      = var.google_project_id
-  org_id          = var.google_organization_id
-}
-
 resource "google_project_service" "cloud-iot-apis" {
-  project = google_project.ferrarimarco-iot-project.project_id
+  project = var.google_project_id
   service = "cloudiot.googleapis.com"
 
   disable_dependent_services = true
-
-  depends_on = [
-    google_project.ferrarimarco-iot-project
-  ]
 }
 
 resource "google_project_service" "pubsub-apis" {
-  project = google_project.ferrarimarco-iot-project.project_id
+  project = var.google_project_id
   service = "pubsub.googleapis.com"
 
   disable_dependent_services = true
-
-  depends_on = [
-    google_project.ferrarimarco-iot-project
-  ]
 }
 
 resource "google_pubsub_topic" "default-devicestatus" {
   name    = "default-devicestatus"
-  project = google_project.ferrarimarco-iot-project.project_id
+  project = var.google_project_id
 
   depends_on = [
-    google_project.ferrarimarco-iot-project,
     google_project_service.pubsub-apis
   ]
 }
 
 resource "google_pubsub_topic" "default-telemetry" {
   name    = "default-telemetry"
-  project = google_project.ferrarimarco-iot-project.project_id
+  project = var.google_project_id
 
   depends_on = [
-    google_project.ferrarimarco-iot-project,
     google_project_service.pubsub-apis
   ]
 }
 
 resource "google_cloudiot_registry" "home-registry" {
   name    = "home-registry"
-  project = google_project.ferrarimarco-iot-project.project_id
+  project = var.google_project_id
 
   depends_on = [
-    google_project.ferrarimarco-iot-project,
     google_project_service.cloud-iot-apis,
     google_project_service.pubsub-apis
   ]
