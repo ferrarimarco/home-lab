c97ca0a07997f6787d3d0c12388c56e269c6d175
Marco Ferrari <ferrari.marco@gmail.com>
2020-08-12 16:30:22 +0200

    Implement NVS blob save and existence check

--- provisioning/esp32/smart_desk/components/storage_manager/nvs_manager.c
+++ provisioning/esp32/smart_desk/components/storage_manager/nvs_manager.c
@@ -4,8 +4,45 @@
 #include "nvs_flash.h"
 #include "nvs_manager.h"
 
+static const char *TAG = "smart_desk";
+
+static esp_err_t open_nvs_handle(const char * namespace, nvs_handle_t* nvs_handle) {
+    esp_err_t err;
+    ESP_LOGI(TAG, "Opening %s NVS namespace...", namespace);
+    err = nvs_open(namespace, NVS_READWRITE, nvs_handle);
+    return err;
+}
+
+bool blob_exists(const char * namespace, const char * key) {
+    ESP_LOGI(TAG, "Checking if a blob with %s key exists in %s namespace...", key, namespace);
+
+    nvs_handle_t nvs_handle;
+    esp_err_t err;
+    bool result = false;
+
+    ESP_ERROR_CHECK(open_nvs_handle(namespace, &nvs_handle));
+
+    ESP_LOGI(TAG, "Checking if the %s namespace contains %s key...", namespace, key);
+    size_t required_size = 0;
+    err = nvs_get_blob(nvs_handle, key, NULL, &required_size);
+    if (err == ESP_ERR_NVS_NOT_FOUND)
+        result = false;
+    else if (err == ESP_OK) {
+        result = true;
+        ESP_LOGI(TAG, "Found %s key in %s namespace. Value size: %u.", key, namespace, required_size);
+    }
+    else
+        ESP_ERROR_CHECK(err);
+
+    ESP_LOGI(TAG, "Closing the NVS storage handle...");
+    nvs_close(nvs_handle);
+
+    return result;
+}
+
 esp_err_t initialize_nvs_flash()
 {
+    ESP_LOGI(TAG, "Initializing the NVS flash...");
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
@@ -15,4 +52,27 @@ esp_err_t initialize_nvs_flash()
     ESP_ERROR_CHECK(ret);
 
     return ret;
+}
+
+esp_err_t save_blob(const char * namespace, const char * key, const void * value, size_t value_size)
+{
+    ESP_LOGI(TAG, "Writing blob in namespace: %s, key: %s...", namespace, key);
+
+    nvs_handle_t nvs_handle;
+    esp_err_t err;
+
+    ESP_ERROR_CHECK(open_nvs_handle(namespace, &nvs_handle));
+
+    ESP_LOGI(TAG, "Writing %s key in %s namespace...", key, namespace);
+    err = nvs_set_blob(nvs_handle, key, value, value_size);
+    if (err != ESP_OK) return err;
+
+    ESP_LOGI(TAG, "Committing NVS changes...");
+    err = nvs_commit(nvs_handle);
+    if (err != ESP_OK) return err;
+
+    ESP_LOGI(TAG, "Closing the NVS storage handle...");
+    nvs_close(nvs_handle);
+
+    return ESP_OK;
 }
--- provisioning/esp32/smart_desk/components/storage_manager/nvs_manager.h
+++ provisioning/esp32/smart_desk/components/storage_manager/nvs_manager.h
@@ -1,3 +1,5 @@
 #include "esp_system.h"
 
+bool blob_exists(const char * namespace, const char * key);
 esp_err_t initialize_nvs_flash();
+esp_err_t save_blob(const char * namespace, const char * key, const void * value, size_t value_size);
