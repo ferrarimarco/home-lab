1aed640ac25befb00a74bc9ca1b07905232c9b0e
Marco Ferrari <ferrari.marco@gmail.com>
2020-09-22 13:58:00 +0200

    Don't load the kubectl config file in the helm provider

--- provisioning/terraform/modules/configuration/helm.tf
+++ provisioning/terraform/modules/configuration/helm.tf
@@ -8,5 +8,7 @@ provider "helm" {
     client_certificate     = base64decode(google_container_cluster.configuration-gke-cluster.master_auth.0.client_certificate)
     client_key             = base64decode(google_container_cluster.configuration-gke-cluster.master_auth.0.client_key)
     cluster_ca_certificate = base64decode(google_container_cluster.configuration-gke-cluster.master_auth.0.cluster_ca_certificate)
+
+    load_config_file = false
   }
 }
