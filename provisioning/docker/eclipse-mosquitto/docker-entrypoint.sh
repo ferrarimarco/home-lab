7a6627736ef846d248813bf2b3eacc8172e11f24
Marco Ferrari <ferrari.marco@gmail.com>
2020-11-02 15:16:25 +0100

    Don't show the JWT token in logs

--- provisioning/docker/eclipse-mosquitto/docker-entrypoint.sh
+++ provisioning/docker/eclipse-mosquitto/docker-entrypoint.sh
@@ -11,5 +11,5 @@ COMMAND_PATH="${1}" && shift
 echo "Generating JWT valid for ${IOT_CORE_PROJECT_ID} project and duration ${IOT_CORE_CREDENTIALS_VALIDITY} seconds from now..."
 IOT_CORE_JWT="$(pyjwt --key="$(cat /etc/cloud-iot-core/keys/private_key.pem)" --alg=RS256 encode iat="$(date +%s)" exp=+"${IOT_CORE_CREDENTIALS_VALIDITY}" aud="${IOT_CORE_PROJECT_ID}")"
 
-echo "Running ${COMMAND_PATH}. Device id: ${IOT_CORE_DEVICE_ID}, Device name: ${IOT_CORE_DEVICE_NAME}, JWT: ${IOT_CORE_JWT}, MQTT timeout: ${IOT_CORE_CREDENTIALS_VALIDITY}"
+echo "Running ${COMMAND_PATH}. Device id: ${IOT_CORE_DEVICE_ID}, Device name: ${IOT_CORE_DEVICE_NAME}, MQTT timeout: ${IOT_CORE_CREDENTIALS_VALIDITY}"
 eval "${COMMAND_PATH}" "--id ${IOT_CORE_DEVICE_ID}" "--pw ${IOT_CORE_JWT}" "-W ${IOT_CORE_CREDENTIALS_VALIDITY}" "--topic /devices/${IOT_CORE_DEVICE_NAME}/commands/#" "--topic /devices/${IOT_CORE_DEVICE_NAME}/config"
