f314bc5945d349778d09aa347e304095273f7fa7
Marco Ferrari <ferrari.marco@gmail.com>
2020-12-30 16:03:07 +0100

    Use jq to fetch the token

--- provisioning/docker/opentelemetry-collector-contrib/docker-entrypoint.sh
+++ provisioning/docker/opentelemetry-collector-contrib/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Loading the bearer token from the metadata server..."
-curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token -H "Metadata-Flavor: Google" >/var/run/secrets/gke-metadata/bearer-token
+curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token -H "Metadata-Flavor: Google" | jq -r '.access_token' >/var/run/secrets/gke-metadata/bearer-token
 
 echo "Starting OpenTelemetry Collector..."
 /otelcontribcol --config /etc/otel/config.yaml
