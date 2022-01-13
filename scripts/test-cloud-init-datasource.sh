c40a779500e0ee53aed8c0f3ea7f37c1eddbf5a9
Marco Ferrari <ferrari.marco@gmail.com>
2022-01-13 13:56:52 +0000

    Remove useless option

--- scripts/test-cloud-init-datasource.sh
+++ scripts/test-cloud-init-datasource.sh
@@ -109,7 +109,7 @@ fi
 decompress_file "${DATASOURCE_IMAGE_PATH}"
 
 DATASOURCE_ISO_MOUNT_PATH="$(mktemp -d)"
-sudo mount -o loop,ro -l  "${DECOMPRESSED_FILE_PATH}" "${DATASOURCE_ISO_MOUNT_PATH}"
+sudo mount -o loop,ro "${DECOMPRESSED_FILE_PATH}" "${DATASOURCE_ISO_MOUNT_PATH}"
 
 echo "Currently mounted file systems:"
 mount
