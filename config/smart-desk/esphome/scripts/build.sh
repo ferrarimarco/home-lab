0feb7e368263369c7170d80040a6ede61fbe483d
Marco Ferrari <ferrari.marco@gmail.com>
2022-02-04 16:00:55 +0100

    secrets.yaml

--- config/smart-desk/esphome/scripts/build.sh
+++ config/smart-desk/esphome/scripts/build.sh
@@ -14,6 +14,7 @@ echo "This script (${SCRIPT_BASENAME}) has been invoked with: $0 $*"
 esphome --verbose config smart-desk.yaml
 
 if [ ! "${CI:-}" = "true" ]; then
+  cp -v secrets-template.yaml secrets.yaml
   esphome run smart-desk.yaml
 else
   echo "Continuous integration environment detected. Compiling the firmware without pushing it to the ESPHome node."
