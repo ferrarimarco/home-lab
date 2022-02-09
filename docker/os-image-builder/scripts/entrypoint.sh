4f5b3862f7b595c3b78b375dad1b0b46fc00925a
Marco Ferrari <ferrari.marco@gmail.com>
2022-02-09 16:33:46 +0000

    Image tag

--- docker/os-image-builder/scripts/entrypoint.sh
+++ docker/os-image-builder/scripts/entrypoint.sh
@@ -221,6 +221,8 @@ if [ -n "${OS_IMAGE_URL}" ]; then
   sha256sum --ignore-missing -c "${OS_IMAGE_CHECKSUM_FILE_PATH}"
 fi
 
+OS_IMAGE_FILE_TAG="${OS_IMAGE_FILE_TAG:-"generic"}"
+
 if [ "${BUILD_TYPE}" = "${BUILD_TYPE_CUSTOMIZE_IMAGE}" ]; then
   IMAGE_ARCHIVE_FILE_PATH="${OS_IMAGE_FILE_PATH}"
   if ! decompress_file "${IMAGE_ARCHIVE_FILE_PATH}"; then
@@ -317,7 +319,7 @@ if [ "${BUILD_TYPE}" = "${BUILD_TYPE_CUSTOMIZE_IMAGE}" ]; then
 
   echo "Unmounting file systems..."
   # We might have "broken" mounts in the mix that point at a deleted image (in case of some odd
-  # build errors). So our "sudo mount" output can look like this:
+  # build errors). So our "mount" output can look like this:
   #
   #     /path/to/our/image.img (deleted) on /path/to/our/mount type ext4 (rw)
   #     /path/to/our/image.img on /path/to/our/mount type ext4 (rw)
@@ -325,9 +327,9 @@ if [ "${BUILD_TYPE}" = "${BUILD_TYPE_CUSTOMIZE_IMAGE}" ]; then
   #
   # so we split on "on" first, then do a whitespace split to get the actual mounted directory.
   # Also we sort in reverse to get the deepest mounts first.
-  for m in $(sudo mount | grep "${TEMP_WORKING_DIRECTORY}" | awk -F " on " '{print $2}' | awk '{print $1}' | sort -r); do
+  for m in $(mount | grep "${TEMP_WORKING_DIRECTORY}" | awk -F " on " '{print $2}' | awk '{print $1}' | sort -r); do
     echo "Unmounting ${m}..."
-    sudo umount "${m}"
+    umount "${m}"
   done
 
   echo "Adding the ${OS_IMAGE_FILE_TAG} tag to the image file..."
