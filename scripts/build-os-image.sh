a9918293ff323e1d23f329e890f0ac05a8373608
Marco Ferrari <ferrari.marco@gmail.com>
2022-02-13 18:19:42 +0000

    fixes

--- scripts/build-os-image.sh
+++ scripts/build-os-image.sh
@@ -21,8 +21,8 @@ echo "Build configuration directory path: ${BUILD_CONFIGURATION_DIRECTORY_PATH}"
 OS_BUILDER_CONTAINER_IMAGE_ID="${OS_BUILDER_CONTAINER_IMAGE_ID:-"ferrarimarco/os-image-builder:latest"}"
 echo "OS Builder container image id: ${OS_BUILDER_CONTAINER_IMAGE_ID}"
 
-INTERACTIVE=$([ -t 0 ] && echo 1 || echo 0)
-if [ "${INTERACTIVE}" = "1" ]; then
+DOCKER_FLAGS=
+if [ -t 0 ]; then
   DOCKER_FLAGS=-it
 fi
 
