c084c179713b05f7f07496896f0961c5531b0c0b
Marco Ferrari <ferrari.marco@gmail.com>
2022-02-25 15:24:17 +0000

    ansible-lint

--- scripts/run-ansible-role-tests.sh
+++ scripts/run-ansible-role-tests.sh
@@ -21,11 +21,19 @@ PIP_REQUIREMENTS_FILE_PATH="${PIP_REQUIREMENTS_FILE_PATH:-"$(pwd)/docker/ansible
 
 (
   create_and_activate_python_virtual_environment "${VIRTUAL_ENVIRONMENT_PATH}" "${PIP_REQUIREMENTS_FILE_PATH}"
+
+  echo "Running ansible-lint to lint ${ANSIBLE_ROLE_PATH}"
+  ANSIBLE_ROLES_PATH="$(dirname "${ANSIBLE_ROLE_PATH}")"
+  export ANSIBLE_ROLES_PATH
+  echo "ANSIBLE_ROLES_PATH: ${ANSIBLE_ROLES_PATH}"
+  ansible-lint -vv "${ANSIBLE_ROLE_PATH}"
+  unset ANSIBLE_ROLES_PATH
+
   PY_COLORS="1"
   export PY_COLORS
   ANSIBLE_FORCE_COLOR="1"
   export ANSIBLE_FORCE_COLOR
-  echo "Testing the role against ${MOLECULE_DISTRO}"
+  echo "Testing the ${ANSIBLE_ROLE_PATH} role against ${MOLECULE_DISTRO}"
   export MOLECULE_DISTRO
   cd "${ANSIBLE_ROLE_PATH}"
   molecule --debug test
