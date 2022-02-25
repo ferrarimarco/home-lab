5f754fc855ab82fec392c6b90137aeb073565030
Marco Ferrari <ferrari.marco@gmail.com>
2022-02-25 15:53:29 +0000

    ansible-lint

--- scripts/run-ansible-role-tests.sh
+++ scripts/run-ansible-role-tests.sh
@@ -13,32 +13,17 @@ echo "This script (name: ${SCRIPT_BASENAME}, directory path: ${SCRIPT_DIRECTORY_
 . "${SCRIPT_DIRECTORY_PATH}/common.sh"
 
 WORKING_DIRECTORY="$(pwd)"
+
 VIRTUAL_ENVIRONMENT_PATH="${WORKING_DIRECTORY}/.venv"
+PIP_REQUIREMENTS_FILE_PATH="${PIP_REQUIREMENTS_FILE_PATH:-"${WORKING_DIRECTORY}/docker/ansible/requirements.txt"}"
+create_and_activate_python_virtual_environment "${VIRTUAL_ENVIRONMENT_PATH}" "${PIP_REQUIREMENTS_FILE_PATH}"
 
-ANSIBLE_ROLE_PATH="${1}"
-MOLECULE_DISTRO="${2}"
-PIP_REQUIREMENTS_FILE_PATH="${PIP_REQUIREMENTS_FILE_PATH:-"$(pwd)/docker/ansible/requirements.txt"}"
+ANSIBLE_DIRECTORY="${WORKING_DIRECTORY}/docker/ansible/etc/ansible"
+ANSIBLE_ROLES_PATH="${ANSIBLE_ROLES_PATH:-"${ANSIBLE_DIRECTORY}/roles"}"
+export ANSIBLE_ROLES_PATH
 
-(
-  create_and_activate_python_virtual_environment "${VIRTUAL_ENVIRONMENT_PATH}" "${PIP_REQUIREMENTS_FILE_PATH}"
+# Running ansible-lint here because super-linter runs it on a file-by-file basis
+ansible-lint -vv "${ANSIBLE_DIRECTORY}"
 
-  echo "Running ansible-lint to lint ${ANSIBLE_ROLE_PATH}"
-  ANSIBLE_ROLES_PATH="$(dirname "${ANSIBLE_ROLE_PATH}")"
-  export ANSIBLE_ROLES_PATH
-  echo "ANSIBLE_ROLES_PATH: ${ANSIBLE_ROLES_PATH}"
-  ansible-lint -vv "${ANSIBLE_ROLE_PATH}"
-  unset ANSIBLE_ROLES_PATH
-
-  PY_COLORS="1"
-  export PY_COLORS
-  ANSIBLE_FORCE_COLOR="1"
-  export ANSIBLE_FORCE_COLOR
-  echo "Testing the ${ANSIBLE_ROLE_PATH} role against ${MOLECULE_DISTRO}"
-  export MOLECULE_DISTRO
-  cd "${ANSIBLE_ROLE_PATH}"
-  molecule --debug test
-)
-
-unset ANSIBLE_ROLE_PATH
-unset MOLECULE_DISTRO
-unset PIP_REQUIREMENTS_FILE_PATH
+unset ANSIBLE_DIRECTORY
+unset ANSIBLE_ROLES_PATH
