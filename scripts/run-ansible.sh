d68bde838e276eff54bfedb088f90ca911481f90
Marco Ferrari <ferrari.marco@gmail.com>
2022-03-29 19:47:31 +0200

    Make workflows reusable (#266)

--- scripts/run-ansible.sh
+++ scripts/run-ansible.sh
@@ -18,15 +18,5 @@ VIRTUAL_ENVIRONMENT_PATH="${WORKING_DIRECTORY}/.venv"
 PIP_REQUIREMENTS_FILE_PATH="${PIP_REQUIREMENTS_FILE_PATH:-"${WORKING_DIRECTORY}/docker/ansible/requirements.txt"}"
 create_and_activate_python_virtual_environment "${VIRTUAL_ENVIRONMENT_PATH}" "${PIP_REQUIREMENTS_FILE_PATH}"
 
-ANSIBLE_DIRECTORY="${WORKING_DIRECTORY}/docker/ansible/etc/ansible"
-ANSIBLE_ROLES_PATH="${ANSIBLE_ROLES_PATH:-"${ANSIBLE_DIRECTORY}/roles"}"
-export ANSIBLE_ROLES_PATH
-
-# Running ansible-lint here because super-linter runs it on a file-by-file basis
-ansible-lint -vv "${ANSIBLE_DIRECTORY}"
-
 echo "Running command: ${1}"
 eval "${1}"
-
-unset ANSIBLE_DIRECTORY
-unset ANSIBLE_ROLES_PATH
