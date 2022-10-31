d7b1028aa474a943a4b2d38431338498cbd42f25
Marco Ferrari <ferrari.marco@gmail.com>
2022-10-31 14:14:48 +0100

    Deploy a DNS server and initial reverse proxy config (#402)

--- scripts/common.sh
+++ scripts/common.sh
@@ -40,7 +40,7 @@ is_python_virtual_environment_up_to_date() {
     PYTHON_VIRTUAL_ENVIRONMENT_CHECK_RETURN_CODE=1
   else
     activate_python_virtual_environment "${1}"
-    if [ "$(pip list --format freeze --outdated | wc -l)" -eq 0 ]; then
+    if [ "$(pip list --outdated | wc -l)" -eq 0 ]; then
       echo "The Python virtual environment (${1}) is up to date."
     else
       echo "The Python virtual environment (${1}) is not up to date. Outdated packages:"
@@ -78,12 +78,12 @@ create_and_activate_python_virtual_environment() {
 
   if [ ! -e "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}" ]; then
     echo "Creating a virtual environment in ${PYTHON_VIRTUAL_ENVIRONMENT_PATH}"
-    python3 -m venv "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}"
+    python3 -m venv "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}" --upgrade-deps
 
     activate_python_virtual_environment "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}"
 
-    echo "Ensure pip, setuptools, and wheel are installed and up to date"
-    pip3 install --upgrade pip setuptools wheel
+    echo "Ensure wheel is installed and up to date"
+    pip3 install --upgrade wheel
 
     if [ -n "${PIP_REQUIREMENTS_PATH}" ] && [ -r "${PIP_REQUIREMENTS_PATH}" ]; then
       echo "Installing dependencies from requirements file: ${PIP_REQUIREMENTS_PATH}"
