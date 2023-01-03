ead80794cec628f69f981274c596b6bbe855c241
Marco Ferrari <ferrari.marco@gmail.com>
2023-01-03 11:37:59 +0000

    Enable verbose output

--- scripts/run-pre-commit.sh
+++ scripts/run-pre-commit.sh
@@ -18,7 +18,7 @@ VIRTUAL_ENVIRONMENT_PATH="${WORKING_DIRECTORY}/.venv-pre-commit"
 PIP_REQUIREMENTS_FILE_PATH="${PIP_REQUIREMENTS_FILE_PATH:-"${WORKING_DIRECTORY}/config/pre-commit/requirements.txt"}"
 create_and_activate_python_virtual_environment "${VIRTUAL_ENVIRONMENT_PATH}" "${PIP_REQUIREMENTS_FILE_PATH}"
 
-COMMAND_TO_RUN="${1:-"pre-commit run --all-files --config config/pre-commit/.pre-commit-config.yaml"}"
+COMMAND_TO_RUN="${1:-"pre-commit run --all-files --config config/pre-commit/.pre-commit-config.yaml --verbose"}"
 
 echo "Running command: ${COMMAND_TO_RUN}"
 eval "${COMMAND_TO_RUN}"
