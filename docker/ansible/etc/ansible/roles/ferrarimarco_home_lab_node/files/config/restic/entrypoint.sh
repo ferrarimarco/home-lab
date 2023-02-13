9576bded1405adde8e3548ef1d832e6e8c94d898
Marco Ferrari <ferrari.marco@gmail.com>
2023-02-13 17:56:18 +0100

    Fix linting errors

--- docker/ansible/etc/ansible/roles/ferrarimarco_home_lab_node/files/config/restic/entrypoint.sh
+++ docker/ansible/etc/ansible/roles/ferrarimarco_home_lab_node/files/config/restic/entrypoint.sh
@@ -9,7 +9,7 @@ SCRIPT_BASENAME="$(basename "${0}")"
 echo "This script (${SCRIPT_BASENAME}) has been invoked with: $0 $*"
 
 echo "Restic repository file: ${RESTIC_REPOSITORY_FILE}"
-RESTIC_REPOSITORY_NAME="$(cat ${RESTIC_REPOSITORY_FILE})"
+RESTIC_REPOSITORY_NAME="$(cat "${RESTIC_REPOSITORY_FILE}")"
 echo "Restic repository: ${RESTIC_REPOSITORY_NAME}"
 
 echo "Checking if the ${RESTIC_REPOSITORY_NAME} Restic repository needs to be initalized."
