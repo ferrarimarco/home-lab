8f645e5c41cd7fafd9001be050cbc75729b4a1e0
Marco Ferrari <ferrari.marco@gmail.com>
2023-02-13 19:13:14 +0100

    Configure restic

--- docker/ansible/etc/ansible/roles/ferrarimarco_home_lab_node/files/config/restic/entrypoint.sh
+++ docker/ansible/etc/ansible/roles/ferrarimarco_home_lab_node/files/config/restic/entrypoint.sh
@@ -9,24 +9,23 @@ SCRIPT_BASENAME="$(basename "${0}")"
 echo "This script (${SCRIPT_BASENAME}) has been invoked with: $0 $*"
 
 echo "Restic repository file: ${RESTIC_REPOSITORY_FILE}"
-RESTIC_REPOSITORY_NAME="$(cat "${RESTIC_REPOSITORY_FILE}")"
-echo "Restic repository: ${RESTIC_REPOSITORY_NAME}"
+echo "Restic repository: ${RESTIC_REPOSITORY}"
 
-echo "Checking if the ${RESTIC_REPOSITORY_NAME} Restic repository needs to be initalized."
+echo "Checking if the ${RESTIC_REPOSITORY} Restic repository needs to be initalized."
 
 if restic snapshots; then
-  echo "The ${RESTIC_REPOSITORY_NAME} repository is already initialized."
+  echo "The ${RESTIC_REPOSITORY} repository is already initialized."
 else
-  echo "Initializing the ${RESTIC_REPOSITORY_NAME} repository"
+  echo "Initializing the ${RESTIC_REPOSITORY} repository"
   restic --verbose init
 fi
 
 if [ "${RESTIC_ENABLE_BACKUP}" = "true" ]; then
-  echo "Backing up ${RESTIC_DIRECTORY_TO_BACKUP}"
-  restic --verbose backup "${RESTIC_DIRECTORY_TO_BACKUP}"
+  echo "Backing up ${RESTIC_DIRECTORIES_TO_BACKUP}"
+  restic --verbose backup "${RESTIC_DIRECTORIES_TO_BACKUP}"
 fi
 
 if [ "${RESTIC_ENABLE_REPOSITORY_CHECK}" = "true" ]; then
-  echo "Checking the integrity of ${RESTIC_REPOSITORY_NAME}"
+  echo "Checking the integrity of ${RESTIC_REPOSITORY}"
   restic --verbose check
 fi
