cf5f31cfd84d999f390410fdf0c61cd314a596ad
Marco Ferrari <ferrari.marco@gmail.com>
2023-04-24 18:23:48 +0200

    Path param

--- scripts/build-arduino-project.sh
+++ scripts/build-arduino-project.sh
@@ -14,3 +14,6 @@ ARDUINO_CLI_CONTAINER_IMAGE_TAG="home-lab/arduino-cli:latest"
 docker build \
   --tag "${ARDUINO_CLI_CONTAINER_IMAGE_TAG}" \
   "${ARDUINO_CLI_CONTAINER_IMAGE_CONTEXT_PATH}"
+
+PROJECT_CONTEXT_PATH="${1}"
+echo "Project context path: ${PROJECT_CONTEXT_PATH}"
