a157706a89f813837fc57a1e91bc0d41ee1b0b76
Marco Ferrari <ferrari.marco@gmail.com>
2023-04-24 19:22:27 +0200

    Board list

--- scripts/build-arduino-project.sh
+++ scripts/build-arduino-project.sh
@@ -17,3 +17,25 @@ docker build \
 
 PROJECT_CONTEXT_PATH="${1}"
 echo "Project context path: ${PROJECT_CONTEXT_PATH}"
+
+ARDUINO_FQBN="${2}"
+echo "Arduino FQBN: ${ARDUINO_FQBN}"
+
+DOCKER_FLAGS=
+if [ -t 0 ]; then
+  DOCKER_FLAGS=-it
+fi
+
+_PROJECT_CONTEXT_DESTINATION_PATH="/workdir/${PROJECT_CONTEXT_PATH}"
+
+echo "Compiling the ${PROJECT_CONTEXT_PATH} sketch for ${ARDUINO_FQBN}"
+docker run \
+  ${DOCKER_FLAGS} \
+  --rm \
+  --volume="/dev:/dev" \
+  --volume="$(pwd)/${PROJECT_CONTEXT_PATH}:${_PROJECT_CONTEXT_DESTINATION_PATH}" \
+  --workdir "${_PROJECT_CONTEXT_DESTINATION_PATH}" \
+  "${ARDUINO_CLI_CONTAINER_IMAGE_TAG}" \
+  "arduino-cli board list"
+
+unset _PROJECT_CONTEXT_DESTINATION_PATH
