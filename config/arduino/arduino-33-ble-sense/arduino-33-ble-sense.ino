5ba0f217deba191df553c2177f96dd7e8e92d283
Marco Ferrari <ferrari.marco@gmail.com>
2023-05-01 12:56:08 +0200

    Read temperature and humidity from HTS221

--- config/arduino/arduino-33-ble-sense/arduino-33-ble-sense.ino
+++ config/arduino/arduino-33-ble-sense/arduino-33-ble-sense.ino
@@ -1,3 +1,4 @@
+#include <Arduino_HTS221.h>
 #include <DHT.h>
 
 #define DHT22_DATA_PIN 2
@@ -12,12 +13,21 @@ void setup() {
 
   Serial.println("Initializing the serial port");
   Serial.begin(9600);
+  // Wait for the serial port to be available
+  while(!Serial);
 
   Serial.print("Initializing the DHT sensor. Data pin: ");
   Serial.print(DHT22_DATA_PIN);
   Serial.print(", DHT sensor type: ");
   Serial.println(DHT_TYPE);
   dht.begin();
+
+  // HTS sensor seems to need some time for setup
+  delay(10);
+  if (!HTS.begin()) {
+    Serial.println("Failed to initialize HTS22 sensor!");
+    while (1);
+  }
 }
 
 void loop() {
@@ -25,27 +35,42 @@ void loop() {
   delay(2000);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  Serial.println("Reading humidity from the DHT sensor");
   float dht22_humidity = dht.readHumidity();
-
   if (isnan(dht22_humidity)) {
     Serial.println("Failed to read humidity from the DHT sensor.");
     return;
   }
 
-  Serial.println("Reading temperature from the DHT sensor");
   float dht22_temperature = dht.readTemperature();
-
   if (isnan(dht22_temperature)) {
     Serial.println("Failed to read temperature from the DHT sensor.");
     return;
   }
 
-  Serial.print("Humidity: ");
+  Serial.print("(DHT22) Humidity: ");
   Serial.print(dht22_humidity);
   Serial.print("% || Temperature: ");
   Serial.print(dht22_temperature);
-  Serial.print("°C ");
+  Serial.print("°C");
 
+  Serial.println();
+
+  float hts22_humidity = HTS.readHumidity();
+  if (isnan(hts22_humidity)) {
+    Serial.println("Failed to read humidity from the HTS221 sensor.");
+    return;
+  }
+
+  float hts22_temperature = HTS.readTemperature();
+  if (isnan(hts22_temperature)) {
+    Serial.println("Failed to read temperature from the HTS221 sensor.");
+    return;
+  }
+
+  Serial.print("(HTS221) Humidity: ");
+  Serial.print(hts22_humidity);
+  Serial.print("% || Temperature: ");
+  Serial.print(hts22_temperature);
+  Serial.print("°C");
   Serial.println();
 }
