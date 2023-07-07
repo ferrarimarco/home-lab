9577db84f17103ed205be3e732e9f3f7ecb994df
Marco Ferrari <ferrari.marco@gmail.com>
2023-07-07 13:23:55 +0300

    Tag restic snapshots (#771)
    Tag restic snapshots and disable cache to avoid reading stale data.

--- docker/restic/entrypoint.sh
+++ docker/restic/entrypoint.sh
@@ -16,22 +16,33 @@ else
 fi
 
 if [ "${RESTIC_ENABLE_BACKUP:-"false"}" = "true" ]; then
-  echo "Backing up ${RESTIC_DIRECTORIES_TO_BACKUP}"
+  echo "Backing up ${RESTIC_DIRECTORIES_TO_BACKUP}. Tags: ${RESTIC_BACKUP_TAGS}"
   # Use eval here because restic interprets quotes literally
-  eval "restic --verbose backup ${RESTIC_DIRECTORIES_TO_BACKUP}"
+  eval "restic --verbose backup --tag ${RESTIC_BACKUP_TAGS} ${RESTIC_DIRECTORIES_TO_BACKUP}"
 fi
 
 if [ "${RESTIC_ENABLE_PRUNE:-"false"}" = "true" ]; then
   echo "Pruning ${RESTIC_DIRECTORIES_TO_BACKUP}"
   # Use eval here because restic interprets quotes literally
-  eval "restic --verbose forget --prune ${RESTIC_FORGET_POLICY}"
+  eval "restic --no-cache --verbose forget --prune ${RESTIC_FORGET_POLICY}"
+fi
+
+if [ "${RESTIC_ENABLE_PRUNE_UNTAGGED_SNAPSHOTS:-"false"}" = "true" ]; then
+  echo "Pruning untagged snapshots"
+  # Use eval here because restic interprets quotes literally
+  eval "restic --no-cache --verbose forget --prune --keep-last 1 --tag ''"
+
+  if [ -n "${RESTIC_TAG_TO_PRUNE:-""}" ]; then
+    echo "Pruning specific tag: ${RESTIC_TAG_TO_PRUNE}"
+    eval "restic --no-cache --verbose forget --prune ${RESTIC_TAG_TO_PRUNE}"
+  fi
 fi
 
 if [ "${RESTIC_ENABLE_REPOSITORY_CHECK:-"false"}" = "true" ]; then
   echo "Checking the integrity of ${RESTIC_REPOSITORY}"
-  restic --verbose check
+  restic --no-cache --verbose check
 fi
 
 # Get some information about the repository
-restic stats
-restic snapshots
+restic --no-cache stats
+restic --no-cache snapshots
