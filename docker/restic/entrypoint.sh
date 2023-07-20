38f178b3a5320c0da8f554ea446521d9a6dda308
Marco Ferrari <ferrari.marco@gmail.com>
2023-07-20 17:49:09 +0200

    Fix restic forget policy (#813)

--- docker/restic/entrypoint.sh
+++ docker/restic/entrypoint.sh
@@ -8,34 +8,38 @@ restic version
 echo "Restic repository: ${RESTIC_REPOSITORY}"
 
 echo "Checking if the ${RESTIC_REPOSITORY} Restic repository needs to be initalized."
-if restic --verbose snapshots; then
+if restic snapshots --verbose; then
   echo "The ${RESTIC_REPOSITORY} repository is already initialized."
 else
   echo "Initializing the ${RESTIC_REPOSITORY} repository"
-  restic --verbose init
+  restic init --verbose
 fi
 
 if [ "${RESTIC_ENABLE_BACKUP:-"false"}" = "true" ]; then
   echo "Backing up ${RESTIC_DIRECTORIES_TO_BACKUP}. Tags: ${RESTIC_BACKUP_TAGS}"
   # Use eval here because restic interprets quotes literally
-  eval "restic --verbose backup --tag ${RESTIC_BACKUP_TAGS} ${RESTIC_DIRECTORIES_TO_BACKUP}"
+  eval "restic backup --tag ${RESTIC_BACKUP_TAGS} --verbose ${RESTIC_DIRECTORIES_TO_BACKUP}"
 fi
 
 if [ "${RESTIC_ENABLE_PRUNE:-"false"}" = "true" ]; then
-  echo "Pruning ${RESTIC_DIRECTORIES_TO_BACKUP}"
+  echo "Pruning ${RESTIC_REPOSITORY}"
   # Use eval here because restic interprets quotes literally
-  eval "restic --no-cache --verbose forget --prune ${RESTIC_FORGET_POLICY}"
+  eval "restic forget --no-cache --prune --verbose ${RESTIC_FORGET_POLICY}"
 fi
 
 if [ "${RESTIC_ENABLE_PRUNE_UNTAGGED_SNAPSHOTS:-"false"}" = "true" ]; then
   echo "Pruning untagged snapshots"
-  # Use eval here because restic interprets quotes literally
-  eval "restic --no-cache --verbose forget --prune --keep-last 1 --tag ''"
+  # As a safety measure, Restic doesn't remove snapshots if the keep policy results in an
+  # empty set. That's why we keep the last snapshot, to remove manually
+  # (see the RESTIC_SNAPSHOT_TO_PRUNE block below).
 
-  if [ -n "${RESTIC_TAG_TO_PRUNE:-""}" ]; then
-    echo "Pruning specific tag: ${RESTIC_TAG_TO_PRUNE}"
-    eval "restic --no-cache --verbose forget --prune ${RESTIC_TAG_TO_PRUNE}"
-  fi
+  # Use eval here because restic interprets quotes literally
+  eval "restic forget --keep-last 1 --no-cache --prune --tag '' --verbose"
+fi
+
+if [ -n "${RESTIC_SNAPSHOT_TO_PRUNE:-""}" ]; then
+  echo "Forgetting and pruning specific snapshot: ${RESTIC_SNAPSHOT_TO_PRUNE}"
+  eval "restic forget --no-cache --prune --verbose ${RESTIC_SNAPSHOT_TO_PRUNE}"
 fi
 
 if [ "${RESTIC_ENABLE_REPOSITORY_CHECK:-"false"}" = "true" ]; then
@@ -43,6 +47,6 @@ if [ "${RESTIC_ENABLE_REPOSITORY_CHECK:-"false"}" = "true" ]; then
   restic --no-cache --verbose check
 fi
 
-# Get some information about the repository
-restic --no-cache stats
-restic --no-cache snapshots
+echo "Get information about the ${RESTIC_REPOSITORY} repository"
+restic stats --no-cache
+restic snapshots --no-cache
