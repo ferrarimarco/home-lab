e4e1fd7012577cec51cd25f8b44e4fb026478266
Marco Ferrari <ferrari.marco@gmail.com>
2023-09-10 19:33:48 +0300

    Fully containerize and automate Ansible (#875)

--- scripts/common.sh
+++ scripts/common.sh
@@ -5,6 +5,8 @@ set -o nounset
 
 # shellcheck disable=SC2034
 ERR_ARGUMENT_EVAL=2
+# shellcheck disable=SC2034
+ERR_ANSIBLE_MISSING_PASSWORD_FILE=3
 
 is_command_available() {
   if command -v "${1}" >/dev/null 2>&1; then
