024a05c7fece994a8a8f202acf243df77add2d4c
Marco Ferrari <ferrari.marco@gmail.com>
2023-10-28 19:39:37 +0200

    Create venv only when necessary

---
+++ config/ansible/roles/ferrarimarco_home_lab_bootstrap/files/host-configuration/setup-home-lab-host-configuration.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+_PIP_REQUIREMENTS_FILE_HASH_PATH="${PYTHON_VIRTUAL_ENVIRONMENT_PATH}/.pip_requirements_hash"
+
+if [ -f "${_PIP_REQUIREMENTS_FILE_PATH}" ]; then
+  echo "[Error] Cannot find the pip requirements file: ${_PIP_REQUIREMENTS_FILE_PATH}"
+  exit 1
+fi
+
+if [ ! -f "${_PIP_REQUIREMENTS_FILE_HASH_PATH}" ] || [ "$(sha256sum "${HOST_CONFIGURATION_SERVICE_PIP_REQUIREMENTS_FILE_PATH}")" != "$(cat "${_PIP_REQUIREMENTS_FILE_HASH_PATH}")" ]; then
+  python -m venv "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}" --upgrade-deps
+  sha256sum "${HOST_CONFIGURATION_SERVICE_PIP_REQUIREMENTS_FILE_PATH}" >"${_PIP_REQUIREMENTS_FILE_HASH_PATH}"
+
+  # shellcheck source=/dev/null
+  . "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}/bin/activate"
+
+  pip install --upgrade wheel
+  pip install -r "${HOST_CONFIGURATION_SERVICE_PIP_REQUIREMENTS_FILE_PATH}"
+fi
