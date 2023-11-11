05ea5c9c3f607460c09d576d9e49c85fb6349567
Marco Ferrari <ferrari.marco@gmail.com>
2023-11-11 18:48:33 +0100

    Reuse existing file

--- scripts/configure-home-lab-host.sh
+++ scripts/configure-home-lab-host.sh
@@ -11,6 +11,7 @@ if [ -f "${_PIP_REQUIREMENTS_FILE_PATH}" ]; then
 fi
 
 if [ ! -f "${_PIP_REQUIREMENTS_FILE_HASH_PATH}" ] || [ "$(sha256sum "${HOST_CONFIGURATION_SERVICE_PIP_REQUIREMENTS_FILE_PATH}")" != "$(cat "${_PIP_REQUIREMENTS_FILE_HASH_PATH}")" ]; then
+  rm -rfv "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}"
   python -m venv "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}" --upgrade-deps
   sha256sum "${HOST_CONFIGURATION_SERVICE_PIP_REQUIREMENTS_FILE_PATH}" >"${_PIP_REQUIREMENTS_FILE_HASH_PATH}"
 
@@ -20,3 +21,7 @@ if [ ! -f "${_PIP_REQUIREMENTS_FILE_HASH_PATH}" ] || [ "$(sha256sum "${HOST_CONF
   pip install --upgrade wheel
   pip install -r "${HOST_CONFIGURATION_SERVICE_PIP_REQUIREMENTS_FILE_PATH}"
 fi
+
+# Activate the virtual environment in case we reuse an existing one
+# shellcheck source=/dev/null
+. "${PYTHON_VIRTUAL_ENVIRONMENT_PATH}/bin/activate"
