035b010faad859890fa07fc1824bf8322b7c9de2
Marco Ferrari <ferrari.marco@gmail.com>
2024-08-20 18:41:36 +0200

    chore: fix linting errors

--- .github/renovate-global-config.js
+++ .github/renovate-global-config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  gitAuthor: "github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+  gitAuthor:
+    "github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
   onboarding: false,
   platform: "github",
   repositories: ["ferrarimarco/home-lab"],
