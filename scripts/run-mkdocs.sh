fce5f65da3d02cac66141415fb0f40d08686eb29
Marco Ferrari <ferrari.marco@gmail.com>
2025-06-16 22:16:10 +0200

    chore: parametrize mkdocs script (#1899)

--- scripts/run-mkdocs.sh
+++ scripts/run-mkdocs.sh
@@ -11,13 +11,25 @@ set -o pipefail
 MKDOCS_CONTAINER_IMAGE_VERSION="9.5.41"
 MKDOCS_CONTAINER_IMAGE="squidfunk/mkdocs-material:${MKDOCS_CONTAINER_IMAGE_VERSION}"
 
+SCRIPT_DIRECTORY_PATH=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+echo "Script directory path: ${SCRIPT_DIRECTORY_PATH}"
+
 echo "Running mkdocs: ${MKDOCS_CONTAINER_IMAGE}"
 
 SUBCOMMAND="${1}"
 echo "Subcommand: ${SUBCOMMAND}"
+shift 1
 
-MKDOCS_CONFIG_FILE_DIRECTORY_NAME="${2}"
+MKDOCS_CONFIG_FILE_DIRECTORY_NAME="${1}"
 echo "Mkdocs config file directory name: ${MKDOCS_CONFIG_FILE_DIRECTORY_NAME}"
+shift 1
+
+MKDOCS_SOURCE_DIRECTORY_PATH="${1}"
+echo "Mkdocs source directory path: ${MKDOCS_SOURCE_DIRECTORY_PATH}"
+shift 1
+
+MKDOCS_DESTINATION_DIRECTORY_PATH="${1}"
+echo "Mkdocs destination directory path: ${MKDOCS_CONFIG_FILE_DIRECTORY_NAME}"
 
 RUN_CONTAINER_COMMAND=(
   docker run
@@ -31,28 +43,36 @@ if [ -t 0 ]; then
   )
 fi
 
-RUN_CONTAINER_COMMAND+=(
-  --name "mkdocs"
-  --publish "8000:8000"
-  --volume "$(pwd)":/docs
-  --volume /etc/localtime:/etc/localtime:ro
-  "${MKDOCS_CONTAINER_IMAGE}"
-)
-
-MKDOCS_CONFIG_FILE_PATH="config/mkdocs/${MKDOCS_CONFIG_FILE_DIRECTORY_NAME}/mkdocs.yaml"
+if ! MKDOCS_CONFIG_FILE_PATH="$(readlink -f "${SCRIPT_DIRECTORY_PATH}/..")/config/mkdocs/${MKDOCS_CONFIG_FILE_DIRECTORY_NAME}/mkdocs.yaml"; then
+  echo "Error while initializing MKDOCS_CONFIG_FILE_PATH"
+fi
+echo "Mkdocs config file path: ${MKDOCS_CONFIG_FILE_PATH}"
 if [[ ! -f "${MKDOCS_CONFIG_FILE_PATH}" ]]; then
   echo "Mkdocs config file (${MKDOCS_CONFIG_FILE_PATH}) doesn't exist or is not readable"
   exit "${ERR_ARGUMENT_EVAL}"
 fi
 
+MKDOCS_CONFIG_FILE_DESTINATION_PATH="/config/mkdocs.yaml"
+
+RUN_CONTAINER_COMMAND+=(
+  --name "mkdocs"
+  --publish "8000:8000"
+  --volume "${MKDOCS_CONFIG_FILE_PATH}":"${MKDOCS_CONFIG_FILE_DESTINATION_PATH}"
+  --volume "${MKDOCS_SOURCE_DIRECTORY_PATH}":/docs
+  --volume "${MKDOCS_DESTINATION_DIRECTORY_PATH}":/dest
+  --volume /etc/localtime:/etc/localtime:ro
+  "${MKDOCS_CONTAINER_IMAGE}"
+)
+
 DEFAULT_MKDOCS_ARGS=(
-  --config-file "${MKDOCS_CONFIG_FILE_PATH}"
+  --config-file "${MKDOCS_CONFIG_FILE_DESTINATION_PATH}"
 )
 
 if [[ "${SUBCOMMAND}" == "serve" ]]; then
   RUN_CONTAINER_COMMAND+=(
     "serve"
     "--dev-addr=0.0.0.0:8000"
+    "--strict"
     "${DEFAULT_MKDOCS_ARGS[@]}"
   )
 elif [[ "${SUBCOMMAND}" == "build" ]]; then
