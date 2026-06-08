f9845ddc15775e7cf831c8cb746eed257b86a335
Marco Ferrari <ferrari.marco@gmail.com>
2026-06-08 22:11:08 +0200

    chore: configure home-lab-platform group

--- config/terraform/200-proxmox-iac-automation-init/main.tf
+++ config/terraform/200-proxmox-iac-automation-init/main.tf
@@ -1,5 +1,4 @@
 locals {
   base_directory_path         = "${path.module}/.."
   environments_directory_path = "${local.base_directory_path}/environments"
-  templates_directory_path    = "${path.module}/templates"
 }
