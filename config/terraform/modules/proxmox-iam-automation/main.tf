e3920a8c536d54ed5ca79c095e7e4b8aadf4ef9d
Marco Ferrari <ferrari.marco@gmail.com>
2026-08-25 21:53:58 +0200

    chore: refactor in-line proxmox acl

--- config/terraform/modules/proxmox-iam-automation/main.tf
+++ config/terraform/modules/proxmox-iam-automation/main.tf
@@ -27,13 +27,14 @@ resource "proxmox_acl" "terraform_automation_writer_user_token" {
   token_id = proxmox_user_token.terraform_automation_writer_api_token.id
 }
 
-resource "proxmox_virtual_environment_user" "terraform_automation_writer" {
-  acl {
-    path      = "/"
-    propagate = true
-    role_id   = proxmox_virtual_environment_role.terraform_automation_writer.role_id
-  }
+resource "proxmox_acl" "terraform_automation_writer_user" {
+  path      = "/"
+  propagate = true
+  role_id   = proxmox_virtual_environment_role.terraform_automation_writer.role_id
+  user_id   = proxmox_virtual_environment_user.terraform_automation_writer.user_id
+}
 
+resource "proxmox_virtual_environment_user" "terraform_automation_writer" {
   comment = "Managed by Terraform"
   user_id = "terraform-automation-writer@pam"
 }
